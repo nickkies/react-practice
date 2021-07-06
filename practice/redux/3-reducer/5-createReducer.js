ba23ec6cb399c2eb4f93af7a65f36b9cbea6493f
YoonSung Kim <opqudk@gmail.com>
2021-07-06 16:14:10 +0900

    createReducer

---
+++ practice/redux/3-reducer/5-createReducer.js
@@ -0,0 +1,35 @@
+/* function reducer(state = INITIAL_STATE, action) {
+  return produce(state, draft => {
+    switch ( action.type ) {
+      case ADD:
+        draft.todos.push(action.todo);
+        break;
+      case REMOVE_ALL:
+        draft.todos = [];
+        break;
+      case REMOVE:
+        draft.todos = draft.todos.filter(todo => todo.id !== action.id);
+        break;
+      default:
+        break;
+    }
+  });
+} */
+
+const reducer = createReducer(INITIAL_STATE, {
+  [ADD]: (state, action) => state.todos.push(action.todo),
+  [REMOVE_ALL]: state => (state.todos = []),
+  [REMOVE]: (state, action) =>
+    (state.todos = state.todos.filter(todo => todo.id !== action.id )),
+});
+
+function createReducer(initialState, handlerMap) {
+  return function (state = initialState, action) {
+    return produce(state, draft => {
+      const handler = handlerMap[action.type];
+      if ( handler ) {
+        handler(draft, action);
+      }
+    });
+  };
+}
