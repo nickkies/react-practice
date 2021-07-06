87b69852691dd3d19ff8b0b595a1ee0a07becaa2
YoonSung Kim <opqudk@gmail.com>
2021-07-06 15:50:02 +0900

    reducer, produce

---
+++ practice/redux/3-reducer/2-2.js
@@ -0,0 +1,19 @@
+const state = {
+  user: {
+    name: 'nick',
+    friends: [
+      {
+        name: 'jessy',
+        age: 29,
+      },
+      {
+        name: 'jun',
+        age: 5,
+      },
+    ],
+  },
+  products: [],
+};
+
+prevState.user === nextState.user
+prevState.products === nextState.products
